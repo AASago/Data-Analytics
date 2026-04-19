d68a35b2733f5a1dae3d176c1c042d09fadd94c2
antwaushea sago <asago@my.yearupunited.org>
2026-04-19 02:28:48 -0500

    Ex1B_first_query.sql

--- Week-02/Ex1B_first_query.sql
+++ Week-02/Ex1B_first_query.sql
@@ -1,3 +1,10 @@
 /*
 sakila is a database used to collect information about movies and customers used by movie stores
 /*
+
+USE sakila;
+SELECT * FROM actor; -- Retrieved 200 records 
+
+SELECT * FROM film; Retrieved 1000 records 
+
+
