43ef4cef44ee256ee4c385433385f4b0f6a50eb0
antwausheasago <a.sago1996@gmail.com>
2026-06-02 17:20:07 -0500

    updated repofix

---
+++ Week-05/Ex1A_hello.py
@@ -0,0 +1,21 @@
+print('Hello world')
+
+
+message = 'hello world'
+print(message)
+
+
+# Displaying dollars and cents
+#The last number of cents was not outputed 
+dollars = 3
+cents = .50
+print(dollars + cents)
+cents = cents + .25
+print(dollars + cents)
+
+
+d_str = str(3) + " " + ('dollars')
+c_str = str(50) + " " + ('cents')
+print(d_str + " " + c_str) 
+
+
---
+++ Week-05/Ex1B_variable_examples.py
@@ -0,0 +1,23 @@
+customer Id = 11
+name = 'jay'
+gender = 'female'
+dob = 2_5_76
+license = 2706
+
+# full list of reserved words 
+#False	def	if	raise
+#None	del	import	return
+#True	elif	in	try
+#and	else	is	while
+#as	except	lambda	with
+#assert	finally	nonlocal	yield
+#break	for	not
+#class	form	or
+#continue	global	pass
+
+
+#Del- used to delete an object in Python.
+#Import-used to import modules to the project.
+#Elif- else if, checks if some other condition holds when the condition in the if statement is false.
+#Else-tells to perform alternate action if the condition in the if statement is False.
+#Is- is used to check if the two variables are equal.
